bd6739f96f7c31de850b98ace429b49b21766292
Steven <stevenpantaleo@hotmail.com>
2016-10-10 08:42:47 -0400

    MyApi Done

--- app/src/main/java/com/rit/sfp/myapi/MainActivity.java
+++ app/src/main/java/com/rit/sfp/myapi/MainActivity.java
@@ -1,8 +1,8 @@
 package com.rit.sfp.myapi;
 
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -19,12 +19,15 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+
 public class MainActivity extends AppCompatActivity {
+
     EditText emailText;
     TextView responseView;
     ProgressBar progressBar;
     static final String API_KEY = "86d6fcdde6315a64";
     static final String API_URL = "https://api.fullcontact.com/v2/person.json?";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,50 +45,69 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
     class RetrieveFeedTask extends AsyncTask<Void, Void, String> {
-       private Exception exeption;
+
+        private Exception exception;
+        // Declare a string for 'email' here to be used in the doInBackground()
         String email;
 
-        protected void onPreExecute(){
-            //Before going to internet
+// you might want to do some validation on the email here
+
+        protected void onPreExecute() {
+// what to do before the call to the Internet
+
             progressBar.setVisibility(View.VISIBLE);
             responseView.setText("");
-            email=emailText.getText().toString();
+            email = emailText.getText().toString();
         }
-        protected String doInBackground(Void... urls){
-            try{
-                URL url = new URL(API_URL+"email="+email+"&apiKey="+API_KEY);
+
+        protected String doInBackground(Void... urls) {
+// Execute the task in the background
+
+            try {
+                URL url = new URL(API_URL + "email=" + email + "&apiKey=" + API_KEY);
+
                 HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                try{
-                    BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-                    StringBuilder sb = new StringBuilder();
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder stringBuilder = new StringBuilder();
                     String line;
-                    while((line=br.readLine()) != null){
-                        sb.append(line).append("\n");
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line).append("\n");
                     }
-                    br.close();
-                    return sb.toString();
-                }finally{
+                    bufferedReader.close();
+                    return stringBuilder.toString();
+                } finally {
                     urlConnection.disconnect();
                 }
-            }catch(Exception e){
-                Log.e("ERROR",e.getMessage(),e);
+            } catch (Exception e) {
+                Log.e("ERROR", e.getMessage(), e);
                 return null;
             }
         }
-        protected void onPostExecute(String response){
-            if(response == null){
+
+        protected void onPostExecute(String response) {
+// we have the response, turn off progress bar
+            if (response == null) {
                 response = "THERE WAS AN ERROR";
             }
             progressBar.setVisibility(View.GONE);
-            Log.i("INFO",response);
+            Log.i("INFO", response);
             responseView.setText(response);
-            try{
-                JSONObject object = (JSONObject) new JSONTokener(response).nextValue();
-                String requestId = object.getString("requestId");
-                int status = object.getInt("status");
-            }catch(JSONException e){
-                e.printStackTrace();
+
+// Do something with the feed
+            if (!response.equals("THERE WAS AN ERROR")) {
+                try {
+                    Log.i("INFOlol", response);
+                    JSONObject object = (JSONObject) new JSONTokener(response).nextValue();
+                    String requestId = object.getString("requestId");
+                    int status = object.getInt("status");
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
